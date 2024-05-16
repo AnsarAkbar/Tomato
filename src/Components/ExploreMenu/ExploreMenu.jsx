6fa4a1af7209047963aad3128f70101b0192156b
ansarakbar <ansarmahmood298@gmail.com>
2024-05-16 14:36:44 +0500

    set menus

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
+  console.log(category)
   return (
     <div>
-      <div>
-        <h3>Explore our menu</h3>
+      <div className="py-14">
+        <h3 className="text-2xl font-bold font-outfit">Explore our menu</h3>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit,
           soluta maiores tenetur nesciunt et accusantium libero aliquid
           veritatis debitis perspiciatis ducimus nihil porro molestiae eum
           temporibus non cupiditate quibusdam sint.
         </p>
-        {
-            
-        }
+        <div className="flex flex-row m-auto mt-7 gap-3 justify-center">
+          {menu_list.map((value, index) => (
+            <div key={index} onClick={()=>setCategory(value)}>
+              <img
+                src={value.menu_image}
+                alt={value}
+                className="mx-2 m-auto"
+              />
+              <div className="text-center mt-3">{value.menu_name}</div>
+            </div>
+          ))}
+        </div>
       </div>
+      <hr />
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
+import React, { useState } from 'react'
+import Header from '../../Components/Banner/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import MenuItems from '../../Components/ExploreMenu/MenuItems'
 
 const Home = () => {
+  const [category,setCategory]=useState("All")
   return (
     <>
         <Header />
+        <ExploreMenu  setCategory={setCategory}/>
+        <MenuItems category={category}/>
     </>
   )
 }
