15204702c6b87a1f803f5c472137eeaa8142a395
ansarakbar <ansarmahmood298@gmail.com>
2024-05-24 23:21:20 +0500

    menu categoryes responsive

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -12,25 +12,29 @@ const ExploreMenu = ({ category, setCategory }) => {
           veritatis debitis perspiciatis ducimus nihil porro molestiae eum
           temporibus non cupiditate quibusdam sint.
         </p>
-        <div className="flex flex-wrap justify-center gap-4">
-          {menu_list.map((value, index) => (
-            <div
-              key={index}
-              onClick={() => setCategory(value)}
-              className="flex flex-col items-center cursor-pointer"
-            >
-              <img
-                src={value.menu_image}
-                alt={value.menu_name}
-                className={`${
-                  category.menu_name === value.menu_name
-                    ? "border-2 border-orange-600 p-[3px] rounded-full"
-                    : ""
-                } w-16 h-16 md:w-20 md:h-20 mx-auto`}
-              />
-              <div className="text-center mt-2 text-sm md:text-base">{value.menu_name}</div>
-            </div>
-          ))}
+        <div className="overflow-x-auto whitespace-nowrap py-4">
+          <div className="inline-flex gap-4">
+            {menu_list.map((value, index) => (
+              <div
+                key={index}
+                onClick={() => setCategory(value)}
+                className="flex flex-col items-center cursor-pointer max-md:w-[100px] max-md:h-[100px] overscroll-none"
+              >
+                <img
+                  src={value.menu_image}
+                  alt={value.menu_name}
+                  className={`${
+                    category.menu_name === value.menu_name
+                      ? "border-2 border-orange-600 p-[3px] rounded-full"
+                      : ""
+                  } w-20 h-20 mx-auto`}
+                />
+                <div className="text-center mt-2 text-sm md:text-base">
+                  {value.menu_name}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <hr />
